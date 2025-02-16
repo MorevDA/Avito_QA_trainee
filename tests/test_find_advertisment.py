151c013a3ea4014c7521ef0c2da9e28de309f4ed
Dmitry <morevda1983@gmail.com>
2025-02-16 09:55:22 +0300

    Данные дя поиска и корректировки вынесены в файл test_config модуля config. В связи с этим скорректирован метод для изменения объявления.

--- tests/test_find_advertisment.py
+++ tests/test_find_advertisment.py
@@ -2,22 +2,25 @@
 Модуль для тестирования функционала по поиску объявления
 """
 
-import pages
+
 import pytest
+
+import pages
+from config.test_data import CreateAdvertisement
 
 
 class TestFindAdvertisement:
     placeholder = "Поиск по объявлениям"
-    name = 'ghdydytdf'  # "Название100"
-    text = 'ghdydytdf'  # 'Picture of Название100'
+    name_advertisement:str = CreateAdvertisement.VALUES.get('name')
+    text: str = f'Picture of {name_advertisement}'
 
     @pytest.mark.run(order=2)
     def test_find_advertisement(self, page):
         """Метод для тестирования функционала поиска существующего объявления"""
         pages.index_page.open_index_page(page)
         field = pages.index_page.find_element_by_placeholder(self.placeholder, page)
-        field.fill(self.name)
+        field.fill(self.name_advertisement)
         pages.index_page.click_button_by_role("button", "Найти", page)
-        advertisement = pages.index_page.find_element_by_alt_text(self.name, page)
+        advertisement = pages.index_page.find_element_by_alt_text(self.text, page)
 
         assert advertisement is not None
