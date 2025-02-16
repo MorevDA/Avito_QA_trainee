078290873e15c11cf3504412bb1a51134fc617c4
Dmitry <morevda1983@gmail.com>
2025-02-16 09:36:28 +0300

    Добавлен класс ChangeAdvertisement

--- config/test_data.py
+++ config/test_data.py
@@ -1,4 +1,11 @@
 
 class CreateAdvertisement:
     FIELDS = {'name', 'price', 'description', 'imageUrl'}
-    VALUES = {'name': 'Название1000', 'price': '100', 'description': 'Описание', 'imageUrl': 'URL изображения'}
+    VALUES = {'name': 'Стол Ikea', 'price': '1000', 'description': 'Стол Ikea в отличном состоянии',
+              'imageUrl': 'https://cdn1.ozone.ru/s3/multimedia-1-8/7072025804.jpg'}
+
+
+class ChangeAdvertisement:
+    FIELDS = {'name', 'price', 'description', 'imageUrl'}
+    VALUES = {'name': 'Стул Ikea', 'price': '800', 'description': 'Стул Ikea в отличном состоянии',
+              'imageUrl': 'https://cdn1.ozone.ru/s3/multimedia-u/6836858562.jpg'}
